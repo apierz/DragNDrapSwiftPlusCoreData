8d574b4bf1df0e16533a5c4e88d12b5af1b1236e
Andy <andy@Andys-MacBook-Pro-2.local>
2014-11-09 19:23:44 -0800

    Initial Commit

---
+++ DragAndDrop with CoreDataTests/DragAndDrop_with_CoreDataTests.swift
@@ -0,0 +1,36 @@
+//
+//  DragAndDrop_with_CoreDataTests.swift
+//  DragAndDrop with CoreDataTests
+//
+//  Created by Andy on 11/9/14.
+//  Copyright (c) 2014 Andy Pierz. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class DragAndDrop_with_CoreDataTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
